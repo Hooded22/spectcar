815a8d035358de9df08cebe88aa7a92de9905529
Przemek Sipta <przemek.sipta@gmail.com>
2022-04-21 20:31:59 +0200

    Finish ep for local data

--- server/Controllers/Api/TransactionController.php
+++ server/Controllers/Api/TransactionController.php
@@ -12,8 +12,8 @@ class TransactionController extends BaseController
         $responseData = '';
 
         try {
-            $transacitiomn = new TransactionModel();
-            $responseData = json_encode($transacitiomn->getAll());
+            $transacition = new TransactionModel();
+            $responseData = json_encode($transacition->getAll());
         } catch (Error $e) {
             $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
             $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
@@ -22,7 +22,195 @@ class TransactionController extends BaseController
         }
     }
 
-    public function findByIdAction() {
-        
+    public function findBestSellsByDateAction() {
+
+        if(!$this->validateMethod('GET')) {
+            return;
+        }
+
+        $strErrorDesc = null;
+        $strErrorHeader = null;
+        $responseData = '';
+
+        try {
+            $urlParams = parse_url($_SERVER['REQUEST_URI']);
+            $transaction = new TransactionModel();
+
+            parse_str($urlParams['query'],$params);
+            $date = $params['date'];
+            $date = str_replace("-","/", $date);
+            
+            $responseData = json_encode($transaction->getBestSalesForDate($date));
+            $this->responseData($responseData);
+
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        } finally {
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
+    }
+
+    public function findBestSellsByClientAgeAction() {
+
+        if(!$this->validateMethod('GET')) {
+            return;
+        }
+
+        $strErrorDesc = null;
+        $strErrorHeader = null;
+        $responseData = '';
+
+        try {
+            $urlParams = parse_url($_SERVER['REQUEST_URI']);
+            $transaction = new TransactionModel();
+
+            parse_str($urlParams['query'],$params);
+            $age = $params['age'];
+            
+            $responseData = json_encode($transaction->getBestSalesForAge($age));
+            $this->responseData($responseData);
+
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        } finally {
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
+    }
+
+    public function findBestSellsByClientGenderAction() {
+        if(!$this->validateMethod('GET')) {
+            return;
+        }
+
+        $strErrorDesc = null;
+        $strErrorHeader = null;
+        $responseData = '';
+
+        try {
+            $urlParams = parse_url($_SERVER['REQUEST_URI']);
+            $transaction = new TransactionModel();
+
+            parse_str($urlParams['query'],$params);
+            $gender = $params['gender'];
+            
+            $responseData = json_encode($transaction->getBestSalesForGender($gender));
+            $this->responseData($responseData);
+
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        } finally {
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
+    }
+
+    public function findBestSellsByCityAction() {
+        if(!$this->validateMethod('GET')) {
+            return;
+        }
+
+        $strErrorDesc = null;
+        $strErrorHeader = null;
+        $responseData = '';
+
+        try {
+            $urlParams = parse_url($_SERVER['REQUEST_URI']);
+            $transaction = new TransactionModel();
+
+            if(isset($urlParams['query'])) {
+                parse_str($urlParams['query'],$params);
+                $city = $params['city'];
+
+                $responseData = json_encode($transaction->getBestSalesForCity($city, 10));
+            } else {
+                $result = $transaction->getAllCitiesData();
+                $responseData = [];
+                while($row = $result->fetch_assoc()) {
+                    $responseData = array_merge(
+                        $responseData,
+                        $transaction->getBestSalesForCity($row['transaction_city'], 1)
+                    );
+                }
+                $this->responseData(json_encode($responseData));
+            }
+
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        } finally {
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
+    }
+
+    public function findProfitByCityAction() {
+        if(!$this->validateMethod('GET')) {
+            return;
+        }
+
+        $strErrorDesc = null;
+        $strErrorHeader = null;
+        $responseData = '';
+
+        try {
+            $urlParams = parse_url($_SERVER['REQUEST_URI']);
+            $transaction = new TransactionModel();
+            
+            $responseData = json_encode($transaction->getProfitByCity());
+            $this->responseData($responseData);
+
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        } finally {
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
+    }
+
+    public function findAllCitiesAction() {
+        if(!$this->validateMethod('GET')) {
+            return;
+        }
+
+        $strErrorDesc = null;
+        $strErrorHeader = null;
+        $responseData = '';
+
+        try {
+            $urlParams = parse_url($_SERVER['REQUEST_URI']);
+            $transaction = new TransactionModel();
+            
+            $responseData = json_encode($transaction->getAvailableCities());
+            $this->responseData($responseData);
+
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        } finally {
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
+    }
+
+    public function addByJsonAction() {
+
+        if(!$this->validateMethod('POST')) {
+            return;
+        }
+
+        $transactionJson = json_decode($_POST['data'], true);
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        try {
+            $transaction = new TransactionModel();
+            $responseData = json_encode($transaction->addTransaction($transactionJson));
+            $this->responseData($responseData);
+            echo "Added successfully";
+
+        } catch (Error $e) {
+            $responseData = "ERROR";
+            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            $this->responseData($responseData, $strErrorDesc, $strErrorHeader);
+        }
     }
 }
--- server/Models/TransactionModel.php
+++ server/Models/TransactionModel.php
@@ -4,4 +4,62 @@
             $query = "SELECT * FROM transactions";
             return $this->select($query);
         }
+
+       public function getProfitByCity() {
+            $query = "SELECT transaction_city, COUNT(id) as 'transactions_count', AVG(car_price) as 'average_car_price', SUM(car_price) as 'profit_sum' FROM `transactions` GROUP BY transaction_city";
+            return $this->select($query);
+       }
+
+       public function getBestSalesForCity($city, $limit) {
+           $query = "SELECT * FROM `transactions` WHERE transaction_city LIKE '$city' ORDER BY car_price DESC LIMIT $limit";
+           return $this->select($query);
+       }
+
+       public function getBestSalesForAllCities($cities) {
+            
+       }
+
+       public function getBestSalesForDate($date) {
+            $query = "SELECT * FROM `transactions` WHERE transaction_date LIKE '$date' ORDER BY car_price LIMIT 10";
+            return $this->select($query);
+       }
+
+       public function getBestSalesForAge($age) {
+            $query = "SELECT * FROM `transactions` WHERE client_age = $age ORDER BY car_price LIMIT 10";
+            return $this->select($query);
+       }
+
+       public function getBestSalesForGender($gender) {
+            $query = "SELECT * FROM `transactions` WHERE client_gender LIKE '$gender' ORDER BY car_price LIMIT 10";
+            return $this->select($query);
+       }
+
+       public function getAvailableCities() {
+            $query = "SELECT DISTINCT transaction_city FROM transactions";
+            return $this->select($query);
+       }
+
+       public function getAllCitiesData() {
+         $query = "SELECT DISTINCT transaction_city FROM transactions";
+         return $this->executeStatement($query);
+       }
+
+       public function addTransaction(
+        $transactionJson
+       ) {
+            $client_first_name = $transactionJson['client_first_name'];
+            $client_last_name = $transactionJson['client_last_name'];
+            $client_age = $transactionJson['client_age'];
+            $client_gender = $transactionJson['client_gender'];
+            $transaction_city = $transactionJson['transaction_city'];
+            $transaction_date = $transactionJson['transaction_date'];
+            $car_price = $transactionJson['car_price'];
+            $car_model = $transactionJson['car_model'];
+            $car_make = $transactionJson['car_make'];
+            $car_model_year = $transactionJson['car_model_year'];
+            $car_color = $transactionJson['car_color'];
+
+            $query = "INSERT INTO `transactions`(`id`, `client_first_name`, `client_last_name`, `client_age`, `client_gender`, `transaction_city`, `transaction_date`, `car_price`, `car_model`, `car_make`, `car_model_year`, `car_color`) VALUES (NULL, '$client_first_name', '$client_last_name', '$client_age', '$client_gender', '$transaction_city', '$transaction_date', '$car_price', '$car_model', '$car_make', '$car_model_year', '$car_color')";
+            return $this->executeStatement($query);
+        }
     }
